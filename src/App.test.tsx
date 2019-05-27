0bd5e3e1a20cf8b2a580fdd02cb8577aa4f83128
enpayne <nick.payne89@gmail.com>
2019-05-27 13:19:55 +0200

    rtl start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { cleanup, fireEvent, render, RenderResult } from 'react-testing-library';
-import App, { mockProducts } from './App';
+import { cleanup, render, RenderResult } from 'react-testing-library';
+import App from './App';
 
 afterEach(cleanup);
 
@@ -10,27 +10,6 @@ describe('App', () => {
 
   beforeEach(() => {
     app = render(<App/>);
-  });
-
-  it('should initially display "Cart is empty"', () => {
-    expect(app.queryByText('Cart is empty')).not.toBeNull();
-  });
-
-  it('should initially not display "No Products"', () => {
-    expect(app.queryByText('No Products')).toBeNull();
-  });
-
-  it('should initially display all products', () => {
-    const productNames = app.queryAllByTestId('product-name')
-      .map((element: { innerHTML: any; }) => element.innerHTML);
-
-    expect(productNames).toEqual(mockProducts);
-  });
-
-  it('should add the product to the cart when the add button is clicked', () => {
-    fireEvent.click(app.queryAllByTestId('add-product')[0]);
-    const cartItems = app.queryAllByTestId('cart-item');
-    expect(cartItems.length).toBe(1);
   });
 
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import ProductList from './ProductList';
-import ShoppingCart from './ShoppingCart';
 
-export const mockProducts = ['Tomato', 'Apple', 'Oranges'];
+export const dummyProducts = ['Tomato', 'Apple', 'Orange'];
 
 const App: React.FC = () => {
-  const [products] = useState<string[]>(mockProducts);
-  const [shoppingCart, setShoppingCart] = useState<string[]>([]);
-
-  const onAddProduct = (product: string) => setShoppingCart([...shoppingCart, product]);
 
   return (
-    <div>
-      <ProductList
-        products={products}
-        onAddProduct={onAddProduct}
-      />
-      <ShoppingCart
-        products={shoppingCart}
-      />
-    </div>
+    <div />
   );
 };
 
