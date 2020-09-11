4c6fa6fad122ff387827083e79741cc045c7845d
Christian Abegg <christian.abegg@zuehlke.com>
2020-09-11 13:24:56 +0200

    use term "Cart is empty"

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export interface ShoppingCartProps {
   cart: string[];
 }
 
-export const ShoppingCart: React.FC<ShoppingCartProps> = ({cart}) => {
+export const ShoppingCart: React.FC<ShoppingCartProps> = ({ cart }) => {
   const groupedItems = cart.reduce((acc: any, curr: string) => {
     if (!acc[curr]) {
       acc[curr] = 1;
@@ -46,7 +46,7 @@ export const ShoppingCart: React.FC<ShoppingCartProps> = ({cart}) => {
 
   return (
     <div>
-      {cart.length === 0 && 'Empty Cart'}
+      {cart.length === 0 && 'Cart is empty'}
       {Object.keys(groupedItems).map(key => (
         <div
           key={key}
@@ -64,7 +64,7 @@ interface ProductListProps {
   addProductToCart: (product: string) => void;
 }
 
-const ProductList: React.FC<ProductListProps> = ({products, addProductToCart}) => (
+const ProductList: React.FC<ProductListProps> = ({ products, addProductToCart }) => (
   <div>
     {products.map(product => (
       <div
